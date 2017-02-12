9153d5931004ff4227d9e6e32b46460ec1c9cebe
Nazar Stelmakh (Ubuntu) <nazar.stelmakh@wsu.edu>
2017-02-11 17:36:30 -0800

    Fixed methods. Some cleanup might be needed. Otherwise ready to submit

--- my_search_stub.py
+++ my_search_stub.py
@@ -1,57 +1,79 @@
 from search import *
 from nn_puzzle import *
 import heapq
+import math
 
 class AStar(Search):
+	def __init__(self, problem):
+		Search.__init__(self, problem)
+		self.open_list = []
+		self.closed_list = []
+	
+	def search(self, initial_state):
+		h = self.heuristic(initial_state)
+		node = (h, 0, None, initial_state, None) # node = (heuristic, depth, parent, state, action)
+		self.open_list.append(node)
+		heapq.heapify(self.open_list)
+		#node = heapq.heappop(self.open_list)
 
-    def search(self, initial_state):
-        """Given an initial problem state, encode a search-tree
-        node representing the state and systematically explore
-        the state-space until a goal-state is found.
+		while (len(self.open_list) > 0):
+			node = heapq.heappop(self.open_list)
+			if self.problem.isgoal(node[3]):
+				return node
 
-        Returns:
-         a search-tree node representing a goal state (if found)
-         or None, if no goal is discovered"""
+			self.closed_list.append(node)
+			total_actions = self.problem.actions(node[3])
+			for action in total_actions:
+				child_i = self.child_node(node, action)
+				if (self.is_in_list(self.closed_list, child_i)):
+					continue
+				if (not self.is_in_list(self.open_list, child_i)):
+					if(not self.is_in_list(self.closed_list, child_i)):
+						heapq.heappush(self.open_list, child_i)
+				elif ((node[1]) >= child_i[1]):
+					continue
+				
+	def is_in_list(self, list, node):
+		for i in list:
+			if(node[3] == i[3]):
+				return True
+		return False
 
-        return None
+	def solution(self, node):
 
-    def solution(self, node):
-        """Returns the 'solution' for the specified node in the search tree.
-        That is, this method should return a sequence of tuples:
-        [(state_0, action_0), (state_1, action_1), ..., (state_n, action_n)]
+		path = []
+		p_node = node
+		while (p_node != None):
+			pair = (p_node[3], p_node[4])
+			path.append(pair)
+			p_node = self.parent(p_node)
+		path.reverse()
+		return path
 
-        such that:
-        state_0 is the initial state in the search
-        action_0 is the first action taken
-        action_i is the action taken to transition from state_i to state_{i+1}
-        state_n is the state encapsulated by the 'search_node' argument
-        action_n is None
-        """
-        raise ValueError("This is unimplemented.")
+	def heuristic(self, state):
+		h = 0
+		n = int(math.sqrt(len(state)))
+		for i in range(len(state)):
+			row_i = int(i / n)
+			col_i = i % n
+			row_f = int(state[i] / n)
+			col_f = state[i] % n
+			h += (abs(row_f - row_i) + abs(col_f - col_i))
 
-    def child_node(self, node, action):
-        """Create a child node for this search tree given a parent
-        search tree node and an action to execute. Don't confuse nodes
-        in the search-tree and verticies in the state-space graph."""
-        return None
+		return h
 
-    def parent(self, node):
-        """Return the parent of the specified node in the search tree"""
-        return None
+	def child_node(self, node, action):
+		result = self.problem.result(node[3], action)
+		c_state = result[0]
+		c_h = self.heuristic(c_state)
+		c_node = (c_h, self.depth(node) + 1, node, c_state, action)
+		return c_node
 
-    def depth(self, node):
+	def parent(self, node):
+		return node[2]
 
-
-    	
-        """Determine how deep the search tree node is in the search tree.
-        Consider the initial state (root) to be at depth 0
-
-        Note: this method is NOT required by the Search class interface. But
-        if you wanted to implement BFS or DFS or a variant, you'd likely
-        want such a function.
-        """
-
-        return -1
+	def depth(self, node):
+		return node[1]
         
 class BFS(Search):
     def __init__(self, problem):
@@ -59,16 +81,6 @@ class BFS(Search):
         self.open_list = []
 
     def search(self, initial_state):
-        """Given an initial problem state, encode a search-tree
-        node representing the state and systematically explore
-        the state-space until a goal-state is found.
-
-        Returns:
-         a search-tree node representing a goal state (if found)
-         or None, if no goal is discovered"""
-
-        print("Initial State: ", initial_state)
-
         node = (0, None, initial_state, None) # node = (depth, parent, state, action)
 
         self.open_list.append(node)
@@ -84,69 +96,43 @@ class BFS(Search):
                 heapq.heappush(self.open_list, child_i)
             node = heapq.heappop(self.open_list)
 
-        self.print_Tree(node)
 
-        return None
-
-    def print_Tree(self, child):
-        # node = (depth, parent, state, action)
-        node = child
-        print(node[2])
-        while(not node[0] == 0):
-            print (node[1])
-            node = node[1]
-
+        return node
 
     def solution(self, node):
-        """Returns the 'solution' for the specified node in the search tree.
-        That is, this method should return a sequence of tuples:
-        [(state_0, action_0), (state_1, action_1), ..., (state_n, action_n)]
+        path = []
+        p_node = node
+        while (p_node != None):
+        	pair = (p_node[2], p_node[3])
+        	path.append(pair)
+        	p_node = self.parent(p_node)
+        path.reverse()
+        print(len(path))
+        return path
 
-        such that:
-        state_0 is the initial state in the search
-        action_0 is the first action taken
-        action_i is the action taken to transition from state_i to state_{i+1}
-        state_n is the state encapsulated by the 'search_node' argument
-        action_n is None
-        """
         raise ValueError("This is unimplemented.")
 
     def child_node(self, node, action):
-        """Create a child node for this search tree given a parent
-        search tree node and an action to execute. Don't confuse nodes
-        in the search-tree and verticies in the state-space graph."""
-
         result = self.problem.result(node[2], action)
-
         c_state = result[0]
-
         c_node = (node[0] + 1, node, c_state, action) # node = (depth, parent, state, action) 
 
         return c_node
 
     def parent(self, node):
-        """Return the parent of the specified node in the search tree"""
-        return None
+        return node[1]
 
     def depth(self, node):
+        return node[0]
 
-
-    	
-        """Determine how deep the search tree node is in the search tree.
-        Consider the initial state (root) to be at depth 0
-
-        Note: this method is NOT required by the Search class interface. But
-        if you wanted to implement BFS or DFS or a variant, you'd likely
-        want such a function.
-        """
-
-        return -1
 if __name__ == "__main__":
 
     puzzle = NNPuzzle(3)
-    initial = puzzle.get_shuffled_state(20)
-    solver = BFS(puzzle)
+    initial = puzzle.get_shuffled_state(1000000)
+    solver = AStar(puzzle)
     goal = solver.search(initial)
-    #print("Found goal")
-    #for sa in solver.solution(goal):
-        #puzzle.display(sa)
+    print("Initial State", initial)
+    solver.solution(goal)
+    print("Found goal")
+    for sa in solver.solution(goal):
+        puzzle.display(sa)
