3cd0d0678b4349c12bccc7a2b661719c237a4bd0
Nazar Leonidovich Stelmakh <nazar.stelmakh@wsu.edu>
2017-02-13 11:45:06 -0800

    Fixed AStar with Kevin

--- my_search_stub.py
+++ my_search_stub.py
@@ -7,46 +7,22 @@ import math
 """
 3. ((1, 0, 8, 5, 2, 4, 7, 3, 6), None)
 ((1, 2, 8, 5, 0, 4, 7, 3, 6), 'Down')
-((1, 2, 8, 5, 3, 4, 7, 0, 6), 'Down')
-((1, 2, 8, 5, 3, 4, 0, 7, 6), 'Left')
-((1, 2, 8, 0, 3, 4, 5, 7, 6), 'Up')
-((1, 2, 8, 3, 0, 4, 5, 7, 6), 'Right')
-((1, 2, 8, 3, 4, 0, 5, 7, 6), 'Right')
-((1, 2, 0, 3, 4, 8, 5, 7, 6), 'Up')
-((1, 0, 2, 3, 4, 8, 5, 7, 6), 'Left')
-((0, 1, 2, 3, 4, 8, 5, 7, 6), 'Left')
-((3, 1, 2, 0, 4, 8, 5, 7, 6), 'Down')
-((3, 1, 2, 5, 4, 8, 0, 7, 6), 'Down')
-((3, 1, 2, 5, 4, 8, 7, 0, 6), 'Right')
-((3, 1, 2, 5, 4, 8, 7, 6, 0), 'Right')
-((3, 1, 2, 5, 4, 0, 7, 6, 8), 'Up')
-((3, 1, 0, 5, 4, 2, 7, 6, 8), 'Up')
-((3, 0, 1, 5, 4, 2, 7, 6, 8), 'Left')
-((3, 4, 1, 5, 0, 2, 7, 6, 8), 'Down')
-((3, 4, 1, 0, 5, 2, 7, 6, 8), 'Left')
-((0, 4, 1, 3, 5, 2, 7, 6, 8), 'Up')
-((4, 0, 1, 3, 5, 2, 7, 6, 8), 'Right')
-((4, 1, 0, 3, 5, 2, 7, 6, 8), 'Right')
-((4, 1, 2, 3, 5, 0, 7, 6, 8), 'Down')
-((4, 1, 2, 3, 0, 5, 7, 6, 8), 'Left')
-((4, 1, 2, 3, 6, 5, 7, 0, 8), 'Down')
-((4, 1, 2, 3, 6, 5, 0, 7, 8), 'Left')
-((4, 1, 2, 0, 6, 5, 3, 7, 8), 'Up')
-((0, 1, 2, 4, 6, 5, 3, 7, 8), 'Up')
-((1, 0, 2, 4, 6, 5, 3, 7, 8), 'Right')
-((1, 6, 2, 4, 0, 5, 3, 7, 8), 'Down')
-((1, 6, 2, 0, 4, 5, 3, 7, 8), 'Left')
-((1, 6, 2, 3, 4, 5, 0, 7, 8), 'Down')
-((1, 6, 2, 3, 4, 5, 7, 0, 8), 'Right')
-((1, 6, 2, 3, 0, 5, 7, 4, 8), 'Up')
-((1, 0, 2, 3, 6, 5, 7, 4, 8), 'Up')
-((0, 1, 2, 3, 6, 5, 7, 4, 8), 'Left')
-((3, 1, 2, 0, 6, 5, 7, 4, 8), 'Down')
-((3, 1, 2, 6, 0, 5, 7, 4, 8), 'Right')
-((3, 1, 2, 6, 4, 5, 7, 0, 8), 'Down')
-((3, 1, 2, 6, 4, 5, 0, 7, 8), 'Left')
-((3, 1, 2, 0, 4, 5, 6, 7, 8), 'Up')
-((0, 1, 2, 3, 4, 5, 6, 7, 8), 'Up')
+((1, 2, 8, 5, 4, 0, 7, 3, 6), 'Right')
+((1, 2, 0, 5, 4, 8, 7, 3, 6), 'Up')
+((1, 0, 2, 5, 4, 8, 7, 3, 6), 'Left')
+((1, 4, 2, 5, 0, 8, 7, 3, 6), 'Down')
+((1, 4, 2, 0, 5, 8, 7, 3, 6), 'Left')
+((1, 4, 2, 7, 5, 8, 0, 3, 6), 'Down')
+((1, 4, 2, 7, 5, 8, 3, 0, 6), 'Right')
+((1, 4, 2, 7, 5, 8, 3, 6, 0), 'Right')
+((1, 4, 2, 7, 5, 0, 3, 6, 8), 'Up')
+((1, 4, 2, 7, 0, 5, 3, 6, 8), 'Left')
+((1, 4, 2, 0, 7, 5, 3, 6, 8), 'Left')
+((1, 4, 2, 3, 7, 5, 0, 6, 8), 'Down')
+((1, 4, 2, 3, 7, 5, 6, 0, 8), 'Right')
+((1, 4, 2, 3, 0, 5, 6, 7, 8), 'Up')
+((1, 0, 2, 3, 4, 5, 6, 7, 8), 'Up')
+((0, 1, 2, 3, 4, 5, 6, 7, 8), 'Left')
 
 4. To perform the search on the heap, the 
 runtime to perform this operation is O(n).
@@ -68,34 +44,29 @@ class AStar(Search):
 		solution and it does it really efficiently"""
 
 		h = self.heuristic(initial_state) # get heuristic cost to get to the solution from the initial states
-		node = (h, 0, None, initial_state, None) # node = (heuristic, depth, parent, state, action)
+		node = (h, h, 0, None, initial_state, None) # node = (f, (h)euristic, depth, parent, state, action)
 		self.open_list.append(node) # add node onto a list
 		heapq.heapify(self.open_list) # heapify the list of nodes so least heuristic cost is at the top
 
 		while (len(self.open_list) > 0): # while there is something in the open list
 			node = heapq.heappop(self.open_list) # assign node to be the top node in the list
-			if self.problem.isgoal(node[3]): # if goal/solution is found
+			if self.problem.isgoal(node[4]): # if goal/solution is found
 				return node # return that solution
 
-			self.closed_list.append(node) # add node that has already been checked onto the closed list
-			total_actions = self.problem.actions(node[3]) # get all possible actions from node
+			total_actions = self.problem.actions(node[4]) # get all possible actions from node
 
 			for action in total_actions: # for every action
-
 				child_i = self.child_node(node, action) # get child of that action
 
-				if (self.is_in_list(self.closed_list, child_i)): # if the child has already been visited/looked at
-					continue # move on to the next child
-
-				elif (not self.is_in_list(self.open_list, child_i)): # if child not looked at and not on the list to be looked at 
+				if (not self.is_in_list(self.closed_list, child_i)): # if the child has already been visited/looked at
+					self.closed_list.append(node) # add node that has already been checked onto the closed list
 					heapq.heappush(self.open_list, child_i) # push the child onto the heap
 
-				#elif ((node[1]) >= child_i[1]): # if the depth of the parent node is 
-					#continue
+		return None
 				
 	def is_in_list(self, list, node):
 		for i in list:
-			if(node[3] == i[3]):
+			if(node[4] == i[4]):
 				return True
 		return False
 
@@ -104,7 +75,7 @@ class AStar(Search):
 		path = []
 		p_node = node
 		while (p_node != None):
-			pair = (p_node[3], p_node[4])
+			pair = (p_node[4], p_node[5])
 			path.append(pair)
 			p_node = self.parent(p_node)
 		path.reverse()
@@ -123,17 +94,19 @@ class AStar(Search):
 		return h
 
 	def child_node(self, node, action):
-		result = self.problem.result(node[3], action)
+		result = self.problem.result(node[4], action)
 		c_state = result[0]
 		c_h = self.heuristic(c_state)
-		c_node = (c_h, self.depth(node) + 1, node, c_state, action)
+		c_g = self.depth(node) + 1
+		c_f =  c_h + c_g
+		c_node = (c_f, c_h, c_g, node, c_state, action)
 		return c_node
 
 	def parent(self, node):
-		return node[2]
+		return node[3]
 
 	def depth(self, node):
-		return node[1]
+		return node[2]
         
 class BFS(Search):
     def __init__(self, problem):
@@ -203,4 +176,5 @@ if __name__ == "__main__":
     solver.solution(goal)
     print("Found goal")
     for sa in solver.solution(goal):
-        puzzle.display(sa)
+        print (sa)
+        #puzzle.display(sa)
